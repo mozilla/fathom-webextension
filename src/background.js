13ef98069220124068d8075b87d5f8da198b7c67
Victor Ng <victor@crankycoder.com>
2017-09-29 15:15:16 -0400

    basic message passing from pageAction click to the sidebar is passing data over now.

--- src/background.js
+++ src/background.js
@@ -9,38 +9,38 @@ browser.tabs.onRemoved.addListener(function(tabId, removeInfo) {
 });
 
 browser.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  // First, validate the message's structure
-  if ((msg.from === 'content') && (msg.subject === 'ready')) {
-    // Enable the page-action for the requesting tab
-    // PageActions are disabled by default
-    // Showing a page action means "show the icon", not "open the popup"
-    browser.pageAction.show(sender.tab.id);
+    // First, validate the message's structure
+    if ((msg.from === 'content') && (msg.subject === 'ready')) {
+        // Enable the page-action for the requesting tab
+        // PageActions are disabled by default
+        // Showing a page action means "show the icon", not "open the popup"
+        browser.pageAction.show(sender.tab.id);
 
-    // Save fathom scores into fathomTabInfo
-    fathomTabInfo[sender.tab.id] = msg.scores;
-    console.log(`Added score info ${JSON.stringify(msg.scores)}`);
-  } else if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
-      console.log("Got message from popup for DOMInfo");
-      function onGot(tabs) {
-        for (let tab of tabs) {
-          // tab.url requires the `tabs` permission
-          let tabId = tab.id;
-          let resultData = fathomTabInfo[tabId];
-          console.log(`Sending data back to popup: ${JSON.stringify(resultData)}`);
+        // Save fathom scores into fathomTabInfo
+        fathomTabInfo[sender.tab.id] = msg.scores;
+        console.log(`Added score info ${JSON.stringify(msg.scores)}`);
+    } else if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+        console.log("Got message from popup for DOMInfo");
+        function onGot(tabs) {
+            for (let tab of tabs) {
+                // tab.url requires the `tabs` permission
+                let tabId = tab.id;
+                let resultData = fathomTabInfo[tabId];
+                console.log(`Sending data back to popup: ${JSON.stringify(resultData)}`);
 
-          // Note that we can't use the sendResponse function to send back data.
-          // No idea why this happens, but the promise in the pageAction popup
-          // doesn't seem to get payload we pass into sendResponse.
-          browser.runtime.sendMessage({'from': 'background',
-                                       'subject': 'fathom_data',
-                                       'payload': resultData});
+                // Note that we can't use the sendResponse function to send back data.
+                // No idea why this happens, but the promise in the pageAction popup
+                // doesn't seem to get payload we pass into sendResponse.
+                browser.runtime.sendMessage({'from': 'background',
+                    'subject': 'fathom_data',
+                    'payload': resultData});
 
+            }
         }
-      }
-      function onError(error) {
-        console.log(`Error: ${error}`);
-      }
-      var querying = browser.tabs.query({currentWindow: true, active: true});
-      querying.then(onGot, onError);
-  }
+        function onError(error) {
+            console.log(`Error: ${error}`);
+        }
+        var querying = browser.tabs.query({currentWindow: true, active: true});
+        querying.then(onGot, onError);
+    }
 });
